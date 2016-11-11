5c3123bd1595702e543c5a24c3633b7984ff9708
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-11 15:35:16 -0400

    Complete full CRUD on tasks
    Complete full CRUD on tasks table.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,63 @@
 class TasksController < ApplicationController
 
-  #GET /allTasks
+  #GET /api/allTasks
   def index
     allTasks = Task.all
     render json: allTasks
   end
 
-  #GET /someTask assigned by
-  def indexBy
-    u = User.find(params[:id])
-    render json: u.delegations
-  end
-
-  #GET /someTask assigned to
+  #GET /api/someTask assigned to...
   def indexTo
-    u = User.find(params[:id])
-    render json: u.obligations
+    oneUser = User.find(params[:id])
+    render json: oneUser.obligations
   end
 
+  #GET /api/someTask assigned by...
+  def indexBy
+    oneUser = User.find(params[:id])
+    render json: oneUser.delegations
+  end
+
+  #GET /api/oneTask
+  def show
+  oneTask = Task.find(params[:id])
+  render json: oneTask
+  end
+
+  #POST /api/create oneTask
+  def create
+    oneTask = Task.new(task_params)
+    puts(task_params)
+
+    if oneTask.save
+      render json: oneTask, status: :created, task: oneTask
+    else
+      render json: oneTask.errors, status: :unprocessable_entity
+    end
+  end
+
+  #PATCH/PUT /api/update oneTask
+  def update
+    oneTask = Task.find(params[:id])
+
+    if oneTask.update(task_params)
+      render json: oneTask
+    else
+      render json: oneTask.errors, status: :unprocessable_entity
+    end
+  end
+
+  #DELETE /api/delete oneTask
+  def destroy
+    oneTask = Task.find(params[:id])
+    oneTask.destroy
+
+    if oneTask.destroy
+      render json: {status: 204}
+    else
+      render json: oneTask.errors, status: :unprocessable_entity
+    end
+  end
 
 
   private
@@ -33,10 +73,5 @@ class TasksController < ApplicationController
         :assigned_by
         )
     end
-
-
-
-
-
 
 end
