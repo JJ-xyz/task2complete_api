0a3194ff18d3731f621bd665087f8bc9ec9a71e5
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-11 20:38:02 -0400

    Complete API backend application
    Complete API backend application full CRUD.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate, except: [:login, :create]
 
+   #POST /api/user login
   def login
     user = User.find_by(username: user_params[:username])
     if user && user.authenticate( user_params[:password])
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
     end
   end
 
+   #POST /api/create user
   def create
      user = User.new(user_params)
      if user.save
@@ -20,12 +22,31 @@ class UsersController < ApplicationController
      end
    end
 
+   #GET /api/allUsers
+   def index
+     allUsers = User.all
+     render json: allUsers
+   end
+
+   #GET /api/allUsers
    def show
      user = User.find(params[:id])
-
      render json: {status: 200, user: user}
    end
 
+   #PATCH/PUT /api/update user
+   def update
+     user = User.find(params[:id])
+     #if user.id === logged_user['id']  # uncomment to restict user update
+       if user.update(user_params)
+         render json: user
+       else
+         render json: user.errors, status: :unprocessable_entity
+       end
+     #else                              # uncomment to restict user update
+       #render json: {status: 401, message: 'Profile can only be update by owner' }
+     #end                               # uncomment to restict user update ^^^ also
+   end
 
   private
 
@@ -46,7 +67,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.required(:user).permit(:username, :password)
+      params.required(:user).permit(:username, :password, :email, :e_confirmed)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, path: '/api/users', only: [:create, :show, :update] do
+  resources :users, path: '/api/users', only: [:create, :show, :update, :index] do
    collection do
      post '/login', to: 'users#login'
     end
