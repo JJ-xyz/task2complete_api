87ddf3f643a4ab33f15a58f2146d1aaeff772757
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-20 19:48:04 -0400

    Add seeds file
    Ad some random fake data through seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Task.delete_all
+
+10.times do |i|
+  user_passcode = Faker::Name.first_name
+  User.create(
+    username: user_passcode,
+    password: user_passcode,
+    email: Faker::Internet.free_email(user_passcode),
+    e_confirmed: Faker::Boolean.boolean
+  )
+end
+
+10.times do |i|
+  Task.create(
+    name: Faker::App.name,
+    description: Faker::Hacker.say_something_smart + Faker::Hacker.say_something_smart,
+    date_assigned: Faker::Date.backward(14),
+    date_due: Faker::Date.forward(23),
+    date_completed: "",
+    assigned_to: Faker::Number.between(1, 10),
+    assigned_by: Faker::Number.between(1, 10),
+    is_complete: Faker::Boolean.boolean
+    )
+end
