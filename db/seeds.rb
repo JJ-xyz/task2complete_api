2af54c1198e4532f3eb168b818fd3ee13aa9d603
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-20 21:08:47 -0400

    Complete app
    Complete app ready for deployment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,98 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 User.delete_all
 Task.delete_all
+User.create(
+  username: "Luigi",
+  password: "pass",
+  email: "luigi@nintendo.net",
+  e_confirmed: false
+)
+User.create(
+  username: "Mario",
+  password: "pass",
+  email: "mario@nintendo.net",
+  e_confirmed: false
+)
+User.create(
+  username: "Yoshi",
+  password: "pass",
+  email: "yoshi@nintendo.net",
+  e_confirmed: false
+)
+User.create(
+  username: "Toad",
+  password: "pass",
+  email: "toad@nintendo.net",
+  e_confirmed: false
+)
+User.create(
+  username: "Princes",
+  password: "pass",
+  email: "princes@nintendo.net",
+  e_confirmed: false
+)
 
-10.times do |i|
-  user_passcode = Faker::Name.first_name
-  User.create(
-    username: user_passcode,
-    password: user_passcode,
-    email: Faker::Internet.free_email(user_passcode),
-    e_confirmed: Faker::Boolean.boolean
+Task.create(
+  name: "Network Installation and Setup",
+  description: "Supervise cabling the building.  Perform network equipment installation and setup. Connecting dual fiber optics to external facilities.  Confirm functionality of the satellite backup line.",
+  date_assigned: "2017-01-02",
+  date_due: "2017-03-30",
+  date_completed: "",
+  assigned_to: 3,
+  assigned_by: 2,
+  is_complete: false
   )
-end
-
-10.times do |i|
+Task.create(
+  name: "250 Virtual Servers Network",
+  description: "Perform the installation and configuration of the private cloud. Prepare the development environement first and notify our development team as soon as it is available.",
+  date_assigned: "2016-11-30",
+  date_due: "2017-02-27",
+  date_completed: "",
+  assigned_to: 1,
+  assigned_by: 2,
+  is_complete: true
+  )
   Task.create(
-    name: Faker::App.name,
-    description: Faker::Hacker.say_something_smart + Faker::Hacker.say_something_smart,
-    date_assigned: Faker::Date.backward(14),
-    date_due: Faker::Date.forward(23),
+    name: "Developemnt Team Recruitment",
+    description: "Interview and recruit the software development team.  Pay atention the to new fullstack group coming out of GA",
+    date_assigned: "2016-12-10",
+    date_due: "2017-01-10",
     date_completed: "",
-    assigned_to: Faker::Number.between(1, 10),
-    assigned_by: Faker::Number.between(1, 10),
-    is_complete: Faker::Boolean.boolean
+    assigned_to: 3,
+    assigned_by: 5,
+    is_complete: false
     )
-end
+
+
+
+
+# --- to create test data using Faker ---BEGIN
+# User.delete_all
+# Task.delete_all
+#
+# 10.times do |i|
+#   user_passcode = Faker::Name.first_name
+#   User.create(
+#     username: user_passcode,
+#     password: user_passcode,
+#     email: Faker::Internet.free_email(user_passcode),
+#     e_confirmed: Faker::Boolean.boolean
+#   )
+# end
+#
+# 10.times do |i|
+#   Task.create(
+#     name: Faker::App.name,
+#     description: Faker::Hacker.say_something_smart + Faker::Hacker.say_something_smart,
+#     date_assigned: Faker::Date.backward(14),
+#     date_due: Faker::Date.forward(23),
+#     date_completed: "",
+#     assigned_to: Faker::Number.between(1, 10),
+#     assigned_by: Faker::Number.between(1, 10),
+#     is_complete: Faker::Boolean.boolean
+#     )
+# end
+# --- to create test data using Faker ---END
